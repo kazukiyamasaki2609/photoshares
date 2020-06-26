be0d26af96d3750850596e3c6a1c87d68875343d
EC2 Default User <ec2-user@ip-172-31-59-229.ec2.internal>
2020-06-26 12:08:14 +0000

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'toppages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  resources :users
 end
