2af827f879e451e661d3b9f5b540a7e582437342
EC2 Default User <ec2-user@ip-172-31-59-229.ec2.internal>
2020-06-26 14:21:46 +0000

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users=User.all.order(id: :desc).page(params[:page]).per(30)
+  end
   
   def show
     @user=User.find(params[:id])
@@ -8,7 +14,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
+    @user=User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Photoshares!"
@@ -17,11 +23,38 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
